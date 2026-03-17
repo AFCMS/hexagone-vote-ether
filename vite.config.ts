0975ebee9d17243bd314d6ac86197321f5ea7f58
AFCMS <afcm.contact@gmail.com>
2026-03-17 13:51:40 +0100

    Add SRI plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,23 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { imagetools } from "vite-imagetools";
 import tailwindcss from "@tailwindcss/vite";
+import sri from "vite-plugin-sri-gen";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), imagetools(), tailwindcss()],
+  plugins: [
+    react(),
+    imagetools(),
+    tailwindcss(),
+    sri({
+      algorithm: "sha512",
+      crossorigin: "anonymous",
+      fetchCache: true,
+      fetchTimeoutMs: 5000,
+      skipResources: [],
+      verboseLogging: false,
+    }),
+  ],
   devtools: {
     enabled: false,
   },
